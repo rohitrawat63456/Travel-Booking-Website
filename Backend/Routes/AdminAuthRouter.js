2dfb6f7b343ac69329bd459101aa3b992000c6eb
rohitrawat63456 <122621142+rohitrawat63456@users.noreply.github.com>
2025-07-21 13:16:14 +0530

    files Sync Succesfully

--- Backend/Routes/AdminAuthRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import {postAdminRegister,postAdminLogin} from '../Controllers/AdminAuthController.js';
-const AdminAuthRouter = express.Router();
-AdminAuthRouter.post('/admin/login',postAdminLogin);
-AdminAuthRouter.post('/admin/register',postAdminRegister);
-export default AdminAuthRouter;
--- Backend/Routes/FlightsRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getFlightsData, postAddFlightData } from '../Controllers/FlightsController.js';
-const FilghtRouter  = express.Router();
-FilghtRouter.post('/getFlightsData',getFlightsData);
-FilghtRouter.post('/addFlightData',postAddFlightData);
-export default FilghtRouter;
